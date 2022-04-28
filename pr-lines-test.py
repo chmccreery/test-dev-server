0e7b448ee4f57c0b7317b529ef738fa866a4851b
Clara McCreery <chmccreery@gmail.com>
2022-04-28 16:54:17 -0700

    Update pr-lines-test.py

--- pr-lines-test.py
+++ pr-lines-test.py
@@ -4,10 +4,5 @@ import socket
 6 == 6
 
 s = socket.socket(doesnt, matter)
+print('blah')
 s.bind(('0.0.0.0', 1337))
-
-s = socket.socket(doesnt, matter)
-s.bind(('', 1337))
-
-s = socket.socket(doesnt, matter)
-s.bind(('', 1337))
