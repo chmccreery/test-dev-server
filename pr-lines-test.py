98f4f40e3c6b649c9be49f46c997246d8a8a2294
Clara McCreery <chmccreery@gmail.com>
2022-04-28 15:02:42 -0700

    Update pr-lines-test.py

--- pr-lines-test.py
+++ pr-lines-test.py
@@ -1,11 +1,18 @@
+import socket 
+
+s = socket.socket(doesnt, matter)
+
 5 == 5
 6 == 6
 
-"thisissingleline" == "thisissingleline"
+# ruleid:avoid-bind-to-all-interfaces
+s = socket.socket(doesnt, matter)
+s.bind(('0.0.0.0', 1337))
 
-import pdb;
-pdb.set_trace();
-print("hello world")
+# # ruleid:avoid-bind-to-all-interfaces
+# s = socket.socket(doesnt, matter)
+# s.bind(('::', 1337))
 
-pdb.set_trace();
-20 == 20
+# # ruleid:avoid-bind-to-all-interfaces
+# s = socket.socket(doesnt, matter)
+# s.bind(('',))
