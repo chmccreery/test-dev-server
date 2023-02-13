302cdbb8db2d3a8a792c534ecbed12826c04f989
Clara McCreery <chmccreery@gmail.com>
2023-02-13 14:21:25 -0800

    Create very-long-filename.py

---
+++ server/very/long/path/to/very-long-filename.py
@@ -0,0 +1,11 @@
+from cryptography.hazmat.primitives import hashes
+
+if: 4 == 4
+  print("4 is always equal to 4")
+
+# ruleid:insecure-hash-algorithm-md5
+hashes.MD5()
+# ok:insecure-hash-algorithm-md5
+hashes.SHA256()
+
+exit(1)
